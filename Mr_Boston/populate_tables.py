b44276902fc98b3969b2558519b9e6f8d598b0e5
Orwellis <azhollowaymusic@gmail.com>
2019-03-27 20:56:47 -0500

    Adding to tables

---
+++ Mr_Boston/populate_tables.py
@@ -0,0 +1,105 @@
+from credentials import user, password, rds_host
+import pymysql
+import pandas as pd
+
+liquid_colors = pd.read_csv("./LIquid_Colors_Final.csv")
+
+def populate_liquid_table(liquid_df):
+    conn = pymysql.connect(rds_host, user=user, password=password, connect_timeout=50)
+    cursor = conn.cursor()
+    cursor.execute('USE cocktailproject')
+    for row in range(len(liquid_df)):
+        liquid = liquid_df.iloc[row, 0]
+        hex_color = liquid_df.iloc[row, 1]
+        print(liquid, hex_color)
+        sql = f"INSERT INTO Liquids (Liquid_Name, Color) VALUES ('{(liquid)}', '{(hex_color)}');"
+        cursor.execute(sql)
+    conn.commit()
+    cursor.execute("SELECT * FROM Liquids")
+    data = cursor.fetchall()
+    print(data)
+    conn.close()
+
+# populate_liquid_table(liquid_colors)
+
+garnish_table_sql = "CREATE TABLE Garnishes ( \
+                        Garnish_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Garnish_Name VARCHAR(80) \
+                        );"
+
+category_table_sql = "CREATE TABLE Categories ( \
+                        Category_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Category_Name VARCHAR(80) \
+                        );"
+
+glass_table_sql = "CREATE TABLE Glasses ( \
+                    Glass_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                    , Glass_Name VARCHAR(24) \
+                    , Glass_Size INT \
+                    , Mask VARCHAR(900) \
+                    , Path VARCHAR(3000) \
+                    , Mask_Height FLOAT \
+                    , Mask_Top_Margin FLOAT\
+                    );"
+
+cocktail_table_sql = "CREATE TABLE Cocktails ( \
+                        Cocktail_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Cocktail_Name VARCHAR(80) \
+                        , Glass_ID INT \
+                        , Category_ID INT \
+                        , FOREIGN KEY (Glass_ID) REFERENCES Glasses(Glass_ID) \
+                        , FOREIGN KEY (Category_ID) REFERENCES Categories(Category_ID) \
+                        );"
+
+liquid_instructions_table_sql = "CREATE TABLE Liquid_Instuctions ( \
+                                Liquid_Instruction_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                                , Cocktail_ID INT \
+                                , Liquid_ID INT \
+                                , Measure VARCHAR(24) \
+                                , FOREIGN KEY (Cocktail_ID) REFERENCES Cocktails(Cocktail_ID) \
+                                , FOREIGN KEY (Liquid_ID) REFERENCES Liquids(Liquid_ID) \
+                                );"
+
+garnish_instructions_table_sql = "CREATE TABLE Garnish_Instuctions ( \
+                                    Garnish_Instruction_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                                    , Cocktail_ID INT \
+                                    , Garnish_ID INT \
+                                    , FOREIGN KEY (Cocktail_ID) REFERENCES Cocktails(Cocktail_ID) \
+                                    , FOREIGN KEY (Garnish_ID) REFERENCES Garnishes(Garnish_ID) \
+                                    );"
+
+rating_table_sql = "CREATE TABLE Ratings ( \
+                Rating_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                , Rating INT \
+                , Cocktail_ID INT\
+                , FOREIGN KEY (Cocktail_ID) REFERENCES Cocktails(Cocktail_ID) \
+                );"
+
+
+tables_to_create = [liquid_table_sql, garnish_table_sql, category_table_sql, 
+                    glass_table_sql, cocktail_table_sql, liquid_instructions_table_sql, 
+                    garnish_instructions_table_sql, rating_table_sql]
+
+# print("connecting")
+# # def create_tables():
+# conn = pymysql.connect(rds_host, user=user, password=password, connect_timeout=50)
+# cursor = conn.cursor()
+# cursor.execute('USE cocktailproject')
+# cursor.execute("SHOW TABLES;")
+# tables = cursor.fetchall()
+# cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
+# for table in tables:
+#     cursor.execute(f"DROP TABLE {table[0]};")
+# cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
+# for table in tables_to_create:
+#     cursor.execute(table)
+# cursor.execute("SHOW TABLES;")
+# tables = cursor.fetchall()
+# print(tables)
+# cursor.execute("SELECT * FROM Cocktails;")
+# tables = cursor.fetchall()
+# print(tables)
+# conn.close()
+# print("success")
+
+# # create_tables()
---
+++ Mr_Boston/table_creation.py
@@ -0,0 +1,93 @@
+# from flask import Flask, jsonify, request, redirect
+# from flask_cors import CORS
+from credentials import user, password, rds_host
+import pymysql
+
+
+liquid_table_sql = "CREATE TABLE Liquids ( \
+                        Liquid_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Liquid_Name VARCHAR(80) \
+                        , Color VARCHAR(10) \
+                        );"
+
+garnish_table_sql = "CREATE TABLE Garnishes ( \
+                        Garnish_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Garnish_Name VARCHAR(80) \
+                        );"
+
+category_table_sql = "CREATE TABLE Categories ( \
+                        Category_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Category_Name VARCHAR(80) \
+                        );"
+
+glass_table_sql = "CREATE TABLE Glasses ( \
+                    Glass_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                    , Glass_Name VARCHAR(24) \
+                    , Glass_Size INT \
+                    , Mask VARCHAR(900) \
+                    , Path VARCHAR(3000) \
+                    , Mask_Height FLOAT \
+                    , Mask_Top_Margin FLOAT\
+                    );"
+
+cocktail_table_sql = "CREATE TABLE Cocktails ( \
+                        Cocktail_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                        , Cocktail_Name VARCHAR(80) \
+                        , Glass_ID INT \
+                        , Category_ID INT \
+                        , FOREIGN KEY (Glass_ID) REFERENCES Glasses(Glass_ID) \
+                        , FOREIGN KEY (Category_ID) REFERENCES Categories(Category_ID) \
+                        );"
+
+liquid_instructions_table_sql = "CREATE TABLE Liquid_Instuctions ( \
+                                Liquid_Instruction_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                                , Cocktail_ID INT \
+                                , Liquid_ID INT \
+                                , Measure VARCHAR(24) \
+                                , FOREIGN KEY (Cocktail_ID) REFERENCES Cocktails(Cocktail_ID) \
+                                , FOREIGN KEY (Liquid_ID) REFERENCES Liquids(Liquid_ID) \
+                                );"
+
+garnish_instructions_table_sql = "CREATE TABLE Garnish_Instuctions ( \
+                                    Garnish_Instruction_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                                    , Cocktail_ID INT \
+                                    , Garnish_ID INT \
+                                    , FOREIGN KEY (Cocktail_ID) REFERENCES Cocktails(Cocktail_ID) \
+                                    , FOREIGN KEY (Garnish_ID) REFERENCES Garnishes(Garnish_ID) \
+                                    );"
+
+rating_table_sql = "CREATE TABLE Ratings ( \
+                Rating_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT \
+                , Rating INT \
+                , Cocktail_ID INT\
+                , FOREIGN KEY (Cocktail_ID) REFERENCES Cocktails(Cocktail_ID) \
+                );"
+
+
+tables_to_create = [liquid_table_sql, garnish_table_sql, category_table_sql, 
+                    glass_table_sql, cocktail_table_sql, liquid_instructions_table_sql, 
+                    garnish_instructions_table_sql, rating_table_sql]
+
+print("connecting")
+# def create_tables():
+conn = pymysql.connect(rds_host, user=user, password=password, connect_timeout=50)
+cursor = conn.cursor()
+cursor.execute('USE cocktailproject')
+cursor.execute("SHOW TABLES;")
+tables = cursor.fetchall()
+cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
+for table in tables:
+    cursor.execute(f"DROP TABLE {table[0]};")
+cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
+for table in tables_to_create:
+    cursor.execute(table)
+cursor.execute("SHOW TABLES;")
+tables = cursor.fetchall()
+print(tables)
+cursor.execute("SELECT * FROM Cocktails;")
+tables = cursor.fetchall()
+print(tables)
+conn.close()
+print("success")
+
+# create_tables()
